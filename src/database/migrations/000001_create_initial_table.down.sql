f5fee319e81070232ffd2d67c29d1cb0d4db93d2
masb0ymas <n.fajri@outlook.com>
2024-07-11 16:16:12 +0700

    fix: update migration file

--- src/database/migrations/000001_create_initial_table.down.sql
+++ src/database/migrations/000001_create_initial_table.down.sql
@@ -1,3 +1,7 @@
 DROP EXTENSION IF EXISTS "uuid-ossp";
+
+DROP INDEX IF EXISTS idx_role_name;
+DROP INDEX IF EXISTS idx_user_fullname, idx_user_email, idx_user_token_verify, idx_user_is_active, idx_user_is_blocked, idx_user_role_id;
+
 DROP TABLE IF EXISTS "role";
 DROP TABLE IF EXISTS "user";
--- src/database/migrations/000001_create_initial_table.up.sql
+++ src/database/migrations/000001_create_initial_table.up.sql
@@ -10,6 +10,8 @@ CREATE TABLE IF NOT EXISTS "role"(
   name varchar not null
 );
 
+CREATE INDEX idx_role_name ON "role" (name);
+
 CREATE TABLE IF NOT EXISTS "user"(
   id uuid primary key default uuid_generate_v4(),
   created_at timestamp default now(),
@@ -17,12 +19,20 @@ CREATE TABLE IF NOT EXISTS "user"(
   deleted_at timestamp null,
   fullname varchar not null,
   email varchar not null,
-  password varchar null,
+  password varchar not null,
   phone varchar(20) null,
   token_verify text null,
   address text null,
   is_active boolean not null default false,
   is_blocked boolean not null default false,
   "role_id" uuid not null,
-  foreign key ("role_id") references "role"(id)
+  foreign key ("role_id") references "role"(id),
+  unique(email)
 );
+
+CREATE INDEX idx_user_fullname ON "user" (fullname);
+CREATE INDEX idx_user_email ON "user" (email);
+CREATE INDEX idx_user_token_verify ON "user" (token_verify);
+CREATE INDEX idx_user_is_active ON "user" (is_active);
+CREATE INDEX idx_user_is_blocked ON "user" (is_blocked);
+CREATE INDEX idx_user_role_id ON "user" (role_id);
